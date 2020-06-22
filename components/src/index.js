a92229a31a53638f769a561a843837c2f536d575
David Ong <dodwei.dev@gmail.com>
2020-06-22 17:23:49 +0800

    29 - Specifying images in JSX

--- components/src/index.js
+++ components/src/index.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
+import faker from "faker";
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img src="" alt="avatar" />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00pm</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src={faker.image.avatar()} alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00pm</span>
+          </div>
+          <div className="text">Nice blog post!</div>
         </div>
-    )
+      </div>
+    </div>
+  );
 };
 
-ReactDom.render(<App />, document.querySelector('#root'));
+ReactDom.render(<App />, document.querySelector("#root"));
