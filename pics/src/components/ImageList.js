0c597bc72d6ac274e16d2bf5ce330281f4f726ec
David Ong <dodwei.dev@gmail.com>
2020-07-08 14:52:34 +0800

    97 - Rendering Lists of Components

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const ImageList = ({ images }) => {
-  console.log(images);
-
-  return <div>ImageList</div>;
+  return images.map((image) => {
+    return <img src={image.urls.regular} alt="" />;
+  });
 };
 
 export default ImageList;
