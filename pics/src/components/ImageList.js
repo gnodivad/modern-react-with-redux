c1a2a8370f12d704f2833392ed84e2770c89631a
David Ong <dodwei.dev@gmail.com>
2020-07-08 17:28:02 +0800

    102 - Creating an Image Card Component

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,9 +1,10 @@
 import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = ({ images }) => {
-  const imageList = images.map(({ description, id, urls }) => {
-    return <img alt={description} key={id} src={urls.regular} />;
+  const imageList = images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
   });
 
   return <div className="image-list">{imageList}</div>;
