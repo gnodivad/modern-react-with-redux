ba7a0afe3d1e2945112ec29455b1e3b7a4b163d3
David Ong <dodwei.dev@gmail.com>
2020-07-08 17:08:55 +0800

    100 - Grid CSS

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,9 +1,12 @@
+import "./ImageList.css";
 import React from "react";
 
 const ImageList = ({ images }) => {
-  return images.map(({ description, id, urls }) => {
+  const imageList = images.map(({ description, id, urls }) => {
     return <img alt={description} key={id} src={urls.regular} />;
   });
+
+  return <div className="image-list">{imageList}</div>;
 };
 
 export default ImageList;
