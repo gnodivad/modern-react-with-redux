238497662abec6964c55da633fe64623e9af5a7d
David Ong <dodwei.dev@gmail.com>
2020-07-12 16:35:30 +0800

    148 - Async Code in useEffect

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
 
   useEffect(() => {
-    console.log("hello");
+    const search = async () => {
+      await axios.get("");
+    };
+
+    search();
   }, [term]);
 
   return (
