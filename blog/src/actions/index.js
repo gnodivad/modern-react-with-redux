d943c964c946fe637c888de6ab202464b1dfc604
David Ong <dodwei.dev@gmail.com>
2020-07-24 16:46:49 +0800

    243 - Behind the Scenes of Redux Thunk

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,12 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
-  const response = await jsonPlaceholder.get("./posts");
+export const fetchPosts = () => {
+  return function () {
+    const promise = jsonPlaceholder.get("./posts");
 
-  return {
-    type: "FETCH_POSTS",
-    payload: response,
+    return {
+      type: "FETCH_POSTS",
+      payload: promise,
+    };
   };
 };
