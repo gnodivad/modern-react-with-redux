4e8b91a18e49e39464990ffb13eff4ff26b991cf
David Ong <dodwei.dev@gmail.com>
2020-08-11 16:49:21 +0800

    318 - Highlighting Errored Fields

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -13,10 +13,11 @@ class StreamCreate extends Component {
   }
 
   renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
-        <input {...input} />
+        <input {...input} autoComplete="off" />
         {this.renderError(meta)}
       </div>
     );
@@ -30,7 +31,7 @@ class StreamCreate extends Component {
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form"
+        className="ui form error"
       >
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
