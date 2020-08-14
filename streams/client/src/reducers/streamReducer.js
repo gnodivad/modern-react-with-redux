0ebc62567fc8880372303f40ec2ab10a1e24af86
David Ong <dodwei.dev@gmail.com>
2020-08-14 16:01:31 +0800

    328 - Handling Fetching, Creating and Updating

--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -1,7 +1,17 @@
-import { EDIT_STREAM } from "../actions/types";
+import {
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+} from "../actions/types";
 
 const streamReducer = (state = {}, action) => {
   switch (action.type) {
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
     case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     default:
